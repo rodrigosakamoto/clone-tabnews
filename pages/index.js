aee6d23eebfd842438cba48572063ca6737e3aa9
Rodrigo Sakamoto <48219669+rodrigosakamoto@users.noreply.github.com>
2023-10-04 10:42:59 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>Peixão beiçudo pra alegrar o dia</h1>
+            <img src="https://extra.globo.com/incoming/22399539-f05-12f/w976h550-PROP/blog_fish_lips.jpg" alt="Peixe" />
+        </>
+    )
+}
+
+export default Home;
