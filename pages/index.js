e1f1c490b4bbf70c6440858e36a81c3a469fdfc5
rodrigosakamoto <rodosakamoto@gmail.com>
2023-10-06 19:24:17 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 function Home() {
-    return (
-        <>
-            <h1>Peixão beiçudo pra alegrar o dia</h1>
-            <img src="https://extra.globo.com/incoming/22399539-f05-12f/w976h550-PROP/blog_fish_lips.jpg" alt="Peixe" />
-        </>
-    )
+  return (
+    <>
+      <h1>Peixão beiçudo pra alegrar o dia</h1>
+      <img
+        src="https://extra.globo.com/incoming/22399539-f05-12f/w976h550-PROP/blog_fish_lips.jpg"
+        alt="Peixe"
+      />
+    </>
+  );
 }
 
 export default Home;
