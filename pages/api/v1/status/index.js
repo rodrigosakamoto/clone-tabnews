c8a90725cbc0e94691a80e95ad6e64f34a0227c6
rodrigosakamoto <rodosakamoto@gmail.com>
2023-12-08 15:43:34 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,13 @@
 import { NextApiResponse, NextApiRequest } from "next";
+import database from "../../../../infra/database";
 
 /**
  *
  * @param {NextApiRequest} req
  * @param {NextApiResponse} res
  */
-function status(req, res) {
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum");
   res.status(200).json({
     chave: "São acima da média",
   });
