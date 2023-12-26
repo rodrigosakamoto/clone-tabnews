53a6c2a9e72d9b9b6290447c69a9466ab632870f
rodrigosakamoto <rodosakamoto@gmail.com>
2023-12-26 16:47:05 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,9 +7,34 @@ import database from "infra/database";
  * @param {NextApiResponse} res
  */
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum");
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW SERVER_VERSION;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
   res.status(200).json({
-    chave: "São acima da média",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
