9504708f77e0bf2634c8b9e01166d744df21adb7
rodrigosakamoto <rodosakamoto@gmail.com>
2025-05-15 07:54:43 -0300

    feat: add suport for `PATCH` on `/api/v1/users/[username]

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -5,6 +5,7 @@ import user from "models/user";
 const router = createRouter();
 
 router.get(getHandler);
+router.patch(patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -13,4 +14,12 @@ async function getHandler(req, res) {
   const userFound = await user.findOneByUsername(username);
 
   return res.status(200).json(userFound);
+}
+
+async function patchHandler(req, res) {
+  const username = req.query.username;
+  const userInputValues = req.body;
+
+  const updatedUser = await user.update(username, userInputValues);
+  return res.status(200).json(updatedUser);
 }
