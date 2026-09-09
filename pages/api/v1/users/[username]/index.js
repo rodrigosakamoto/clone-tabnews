d0514cba85c2bc40e3b4556da81ce608ebd9bbfc
rodrigosakamoto <rodosakamoto@gmail.com>
2026-09-09 16:53:47 -0300

    feat: consider `resource` in `authorization` model

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -1,4 +1,6 @@
 import controller from "infra/controller";
+import { ForbiddenError } from "infra/errors";
+import authorization from "models/authorization";
 import user from "models/user";
 import { createRouter } from "next-connect";
 
@@ -20,6 +22,16 @@ async function getHandler(req, res) {
 async function patchHandler(req, res) {
   const username = req.query.username;
   const userInputValues = req.body;
+
+  const userTryingToPatch = req.context.user;
+  const targetUser = await user.findOneByUsername(username);
+
+  if (!authorization.can(userTryingToPatch, "update:user", targetUser)) {
+    throw new ForbiddenError({
+      message: "Você não possui permissão para atualizar outro usuário.",
+      action: "Verifique se você possui a feature necessária para atualizar outro usuário."
+    })
+  }
 
   const updatedUser = await user.update(username, userInputValues);
   return res.status(200).json(updatedUser);
