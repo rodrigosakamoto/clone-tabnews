c6eb29ec583c80c3f398df56e9fbfb7de8cfba05
rodrigosakamoto <rodosakamoto@gmail.com>
2026-09-15 09:47:21 -0300

    test: replace hardcoded `http://localhost:3000` with `${webserver.origin}`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,4 @@
+import webserver from "infra/webserver";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
@@ -7,7 +8,7 @@ beforeAll(async () => {
 describe("GET /api/v1/status", () => {
   describe("Anonymous user", () => {
     test("Retrieving current system status", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/status");
+      const response = await fetch(`${webserver.origin}/api/v1/status`);
       expect(response.status).toBe(200);
 
       const responseBody = await response.json();
@@ -31,7 +32,7 @@ describe("GET /api/v1/status", () => {
         activatedPrivilegedUser.id,
       );
 
-      const response = await fetch("http://localhost:3000/api/v1/status", {
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
         headers: {
           Cookie: `session_id=${privilegedUserSession.token}`,
         },
